84362a5ce5c6f0a1b3fe5b69d00b15184499759c
mohammad-yasser12 <yasserpatla@gmail.com>
2026-06-15 03:02:56 +0400

    fix production URLs (remove localhosSt)

--- backend/Router/MessageRouter.js
+++ backend/Router/MessageRouter.js
@@ -1,11 +1,11 @@
 import express from "express";
-import { sendMessage, getMessages,createOrGetConversation,getUserConversations ,deleteForEveryone,deleteForMe} from "../Controller/messageController.js";
+import { sendMessage, getMessages,createOrGetConversation,getUserConversations ,deleteForEveryone,deleteForMe} from "../Controller/MessageController.js";
 import authMiddleware from "../middleware/authMiddleware.js";
 
 const messageRouter = express.Router();
 
 messageRouter.post("/send", authMiddleware, sendMessage);
-messageRouter.post("/converzations", authMiddleware, createOrGetConversation);
+messageRouter.post("/conversations", authMiddleware, createOrGetConversation);
 messageRouter.get("/conversation", authMiddleware, getUserConversations);
 messageRouter.get("/:conversationId", authMiddleware, getMessages);
 messageRouter.put("/delete/me/:messageId", authMiddleware, deleteForMe);
