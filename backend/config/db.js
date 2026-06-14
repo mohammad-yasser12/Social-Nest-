f59ef62875b91a7db2a8e74d51c66ec481c91e53
mohammad-yasser12 <yasserpatla@gmail.com>
2026-06-15 01:36:42 +0400

    fix env mongo url

--- backend/config/db.js
+++ backend/config/db.js
@@ -6,12 +6,13 @@ dotenv.config();
 const dbUrl = process.env.DB_URL;
 
 const connectToDatabase = async () => {
- try {
-  await mongoose.connect(dbUrl);
-  console.log("MongoDB connected successfully");
-} catch (error) {
-  console.error("MongoDB connection failed:", error.message);
-}
+  try {
+    await mongoose.connect(dbUrl);
+    console.log("MongoDB connected successfully ✅");
+  } catch (error) {
+    console.error("MongoDB connection failed ❌", error);
+    process.exit(1);
+  }
 };
 
 export default connectToDatabase;
