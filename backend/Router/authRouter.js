940706376f5e6569de64866af1e4777047010de8
mohammad-yasser12 <yasserpatla@gmail.com>
2026-06-18 12:45:02 +0400

    debugging

--- backend/Router/authRouter.js
+++ backend/Router/authRouter.js
@@ -13,7 +13,8 @@ import {authMiddleware} from '../middleware/authMiddleware.js';
 const authRouter = express.Router();
 
 
- authRouter.post('/signup', upload.single('profilepicture'), signup);
+//  authRouter.post('/signup', upload.single('profilepicture'), signup);
+authRouter.post("/signup", signup);
  authRouter.post('/login', login);
  
  authRouter.get('/users', authMiddleware, getAllUsers);
